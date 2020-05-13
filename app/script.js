bfec65248c9c1047dcce62a2fbba97daec1969b3
Uskuras <dariususkuras@gmail.com>
2020-05-13 15:59:30 +0300

    added reviews section

--- app/script.js
+++ app/script.js
@@ -1,3 +1,29 @@
+"use strict"
+
+// Initializing swiper for reviews section
+
+var reviewsSwiper = new Swiper ('.reviews-slideshow', {
+    slidesPerView: 1,
+    spaceBetween: 190,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        700: {
+          slidesPerView: 2,
+          spaceBetween: 90,
+        },
+        // when window width is >= 480px
+        1060: {
+          slidesPerView: 3,
+        },
+      }
+});
+
 // Slider
 
 let navBlocks = document.querySelectorAll('.nav-block');
